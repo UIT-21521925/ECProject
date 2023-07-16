edd5f99e1bcb7f8266279336579778001874e46a
LeKieu <21522268@gm.uit.edu.vn>
2023-07-16 21:43:44 +0700

    data overview

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,37 @@ import numpy as np
 from PIL import Image
 from funcs import *
 import streamlit.components.v1 as components
+import matplotlib.pyplot as plt
 import plotly.express as px
-import matplotlib.pyplot as plt 
+import sklearn
+from sklearn.decomposition import TruncatedSVD
+
+# %matplotlib inline
+plt.style.use("ggplot")
+
 
 def main():
-
-
-    sidebar_header =  'H&M Fashion Recommendation System'
+        
+    sidebar_header = '''Ecommerce Product Recommendation System'''
     
-    page_options = ["Data Overview",
-                    "Customer Recommendations",
-                    "Product Captioning"]
+    page_options = ["Data Overview And Visualize Data",
+                    "Find similar items",
+                    "Customer Recommendations",]
     
-    st.sidebar.info(sidebar_header)
+    st.sidebar.title(sidebar_header)
+    
+    # def custom_header(text):
+    #     header_html = "<h1 style='text-align: center; color: white; '>{}</h1>".format(text)
+    #     st.markdown(header_html, unsafe_allow_html=True)
+
+    #     # Sử dụng hàm custom_header() để tạo header tùy chỉnh
+    #     sidebar_header = custom_header("Ecommerce Product Recommendation System")
+    #     st.sidebar.info(sidebar_header)
 
 
+
     
-    page_selection = st.sidebar.radio("Try", page_options)
+    choice = st.sidebar.radio("Content", page_options)
     articles_df = pd.read_csv('articles.csv')
     
     models = ['Similar items based on image embeddings', 
@@ -37,48 +51,63 @@ def main():
 
 #########################################################################################
 #########################################################################################
-    if page_selection == "Data Overview":
-        customers_rcmnds = pd.read_csv('results/customers_rcmnds.csv')
 
-        # Hiển thị thông tin cơ bản về dữ liệu customers_rcmnds
-        st.title('Data Overview - Customer Recommendations')
-        st.markdown('''Develop product recommendations based on data from previous transactions of H&M Groups with 53 online markets and approximately 4,850 stores., as well as from customer and product meta data. 
-            But with too many choices, customers might not quickly find what interests them or what they are looking for, and ultimately, they might not make a purchase. To enhance the shopping experience, product recommendations are key. More importantly, helping customers make the right choices also has a positive implications for sustainability, as it reduces returns, and thereby minimizes emissions from transportation.
-            The available meta data spans from simple data, such as garment type and customer age, to text data from product descriptions, to image data from garment images.This is the data overview for the CSV file: customers_rcmnds.csv.''')
+    if choice == "Find similar items":
 
-        # Thêm biểu đồ tròn
+        articles_rcmnds = pd.read_csv('results/articles_rcmnds.csv')
+        # amazon_ratings = amazon_ratings.dropna()
+        # amazon_ratings.head()
+        # amazon_ratings.shape
+    
+        articles = articles_rcmnds.article_id.unique()
+        get_item = st.sidebar.button('Result')
         
-        customer_counts = customers_rcmnds['customer'].value_counts()
-        fig_pie = px.pie(labels=customer_counts.index, values=customer_counts.values, title="Customer Purchase Count")
-        st.plotly_chart(fig_pie)
-       
-# Đường dẫn tới file CSV
+        if get_item:
+            
+            rand_article = np.random.choice(articles)
+            article_data = articles_rcmnds[articles_rcmnds.article_id == rand_article]
+            rand_article_desc = articles_df[articles_df.article_id == rand_article].detail_desc.iloc[0]
+            image_rcmnds, text_rcmnds, feature_rcmnds, tfrs_rcmnds, combined_rcmnds = get_rcmnds(article_data)
+            
+            rcmnds = (image_rcmnds, text_rcmnds, feature_rcmnds, tfrs_rcmnds, combined_rcmnds)
+            
+            scores = get_rcmnds_scores(article_data)
+            features = get_rcmnds_features(articles_df, image_rcmnds, text_rcmnds, feature_rcmnds, tfrs_rcmnds, combined_rcmnds)
+            images = get_rcmnds_images(image_rcmnds, text_rcmnds, feature_rcmnds, tfrs_rcmnds, combined_rcmnds)
+            detail_descs  = get_rcmnds_desc(articles_df, image_rcmnds, text_rcmnds, feature_rcmnds, tfrs_rcmnds, combined_rcmnds)
+            
+            st.sidebar.image(get_item_image(str(rand_article), width=200, height=300))
+            st.sidebar.write('Article description')
+            st.sidebar.caption(rand_article_desc)
 
-    # Tiêu đề và mô tả chung
-        st.write('Total number of unique customers:', len(customers_rcmnds.customer.unique()))
+            with st.container():     
+                for i, model, image_set, score_set, model_desc, detail_desc_set, features_set, rcmnds_set in zip(range(5), models, images, scores, model_descs, detail_descs, features, rcmnds):
+                    container = st.expander(model, expanded = model == 'Similar items based on image embeddings' or model == 'Similar items based on text embeddings')
+                    with container:
+                        cols = st.columns(7)
+                        cols[0].write('###### Similarity Score')
+                        cols[0].caption(model_desc)
+                        for img, col, score, detail_desc, rcmnd in zip(image_set[1:], cols[1:], score_set[1:], detail_desc_set[1:],  rcmnds_set[1:]):
+                            with col:
+                                st.caption('{}'.format(score))
+                                st.image(img, use_column_width=True)
+                                if model == 'Similar items based on text embeddings':
+                                   st.caption(detail_desc)
+            # popular_products = pd.DataFrame(amazon_ratings.groupby('ProductId')['Rating'].count())
+            # most_popular = popular_products.sort_values('Rating', ascending=False)
+            # most_popular.head(10) 
 
-        # Hiển thị số lượng hàng và cột của DataFrame
-        st.header('Data Shape')
-        st.write('Number of rows:', customers_rcmnds.shape[0])
-        st.write('Number of columns:', customers_rcmnds.shape[1])
+            # most_popular.head(30).plot(kind = "bar")
 
-        # Hiển thị dữ liệu mẫu
-        st.header('Sample Data')
-        st.write(customers_rcmnds.head())
-
-        # Kết thúc ứng dụng
-        st.stop()
-
-
-                              
+                                    
 #########################################################################################
 #########################################################################################
-    if page_selection == "Customer Recommendations":
+    if choice == "Customer Recommendations":
         
         customers_rcmnds = pd.read_csv('results/customers_rcmnds.csv')
         customers = customers_rcmnds.customer.unique()        
         
-        get_item = st.sidebar.button('Get Random Customer')
+        get_item = st.sidebar.button('Result')
         if get_item:
             st.sidebar.write('#### Customer history')
 
@@ -119,6 +148,39 @@ def main():
 
 #########################################################################################  
 #########################################################################################
+#########################################################################################
+    if choice == "Data Overview And Visualize Data":
+        customers_rcmnds = pd.read_csv('results/customers_rcmnds.csv')
+
+        # Hiển thị thông tin cơ bản về dữ liệu customers_rcmnds
+        st.title('Data Overview And Visualize Data - Customer Recommendations')
+        st.markdown('''Develop product recommendations based on data from previous transactions of H&M Groups with 53 online markets and approximately 4,850 stores., as well as from customer and product meta data. 
+            But with too many choices, customers might not quickly find what interests them or what they are looking for, and ultimately, they might not make a purchase. To enhance the shopping experience, product recommendations are key. More importantly, helping customers make the right choices also has a positive implications for sustainability, as it reduces returns, and thereby minimizes emissions from transportation.
+            The available meta data spans from simple data, such as garment type and customer age, to text data from product descriptions, to image data from garment images.This is the data overview for the CSV file: customers_rcmnds.csv.''')
+
+        # Thêm biểu đồ tròn
+        
+        customer_counts = customers_rcmnds['customer'].value_counts()
+        fig_pie = px.pie(labels=customer_counts.index, values=customer_counts.values, title="Customer Purchase Count")
+        st.plotly_chart(fig_pie)
+       
+# Đường dẫn tới file CSV
+
+    # Tiêu đề và mô tả chung
+        st.write('Total number of unique customers:', len(customers_rcmnds.customer.unique()))
+
+        # Hiển thị số lượng hàng và cột của DataFrame
+        st.header('Data Shape')
+        st.write('Number of rows:', customers_rcmnds.shape[0])
+        st.write('Number of columns:', customers_rcmnds.shape[1])
+
+        # Hiển thị dữ liệu mẫu
+        st.header('Sample Data')
+        st.write(customers_rcmnds.head())
+
+        # Kết thúc ứng dụng
+        st.stop()
+
 
 
 if __name__ == '__main__':
